0469dffc18b2fabde23fb279323f86a84fe3f431
Ali Zulfikar <alizulfikar032@gmail.com>
2022-05-30 13:34:10 +0700

    Fix: Color Navbar Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
+    darkmode: 'class',
     content: ["./index.html", "./src/**/*.html", "./node_modules/flowbite/dist/flowbite.js"],
     theme: {
         colors: {
             primary: "#092647",
-            secondary: "#EFA721",
+            'primary-90': "#0b305b",
+            'primary-80': "#0e3c71",
+            'primary-70': "#114988",
+            'primary-60': "#14559f",
+            'primary-50': "#1661b6",
+            'primary-40': "#196dcc",
+            'primary-30': "#1c79e3",
+            'primary-20': "#3386e6",
+            'primary-10': "#4994e9",
+            'primary-5': "#60a1eb",
+            'primary-light': "#8ebcf1",
         },
         screens: {
             'sm': '640px',
@@ -19,7 +30,6 @@ module.exports = {
         extend: {
             fontfamily: {
                 'inter': ['Inter', 'sans-serif'],
-                'poppins': ['Poppins', 'sans-serif'],
             }
         },
     },
